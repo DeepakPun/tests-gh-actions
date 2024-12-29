86ca2baac183ca01db5ee13e88031adbd16ed67d
Deepak Pun <deepakpun@Deepaks-MacBook-Pro.local>
2024-12-28 21:35:51 -0500

    Added two more tests

--- __tests__/sample.test.js
+++ __tests__/sample.test.js
@@ -1,3 +1,19 @@
-const add = require("../utilfuncs/funcs")
+const { add, diff } = require('../utilfuncs/funcs')
 
-test('adds 6 + 6 to equal 12', () => expect(add(6, 6)).toBe(12))
+describe('test add function', () => {
+	test('adds 6 + 6 to equal 12', () => {
+		expect(add(6, 6)).toBe(12)
+	})
+})
+
+describe('test diff function', () => {
+	test('subtract 10 - 4 to equal 6', () => {
+		expect(diff(10, 4)).toBe(6)
+	})
+})
+
+describe('test diff function', () => {
+	test('subtract 20 - 7 to not equal 12', () => {
+		expect(diff(20, 7)).not.toBe(12)
+	})
+})
--- utilfuncs/funcs.js
+++ utilfuncs/funcs.js
@@ -1,3 +1,4 @@
 const add = (a, b) => a + b
+const diff = (a, b) => a - b
 
-module.exports = add
+module.exports = { add, diff }
