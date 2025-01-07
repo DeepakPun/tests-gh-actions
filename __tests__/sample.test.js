0ac61caf84c87d48f8737d9e414af03aabbcbfe3
Deepak Pun <pun.deepak1986@gmail.com>
2025-01-06 19:08:52 -0500

    Updated tests

--- __tests__/sample.test.js
+++ __tests__/sample.test.js
@@ -6,7 +6,7 @@ describe('test utility functions defined in funcs file', () => {
 	})
 
 	test('adds 6 + 6 to equal 12', () => {
-		expect(add(6, 6)).toBe(1200)
+		expect(add(7, 8)).toBe(15)
 	})
 
 	test('adds 6 + 5 to not equal 12', () => {
@@ -17,7 +17,7 @@ describe('test utility functions defined in funcs file', () => {
 		expect(diff(10, 4)).toBe(6)
 	})
 
-	test('subtract 20 - 7 to not equal 12', () => {
-		expect(diff(20, 7)).not.toBe(12)
+	test('subtract 20 - 7 to not equal 13', () => {
+		expect(diff(20, 7)).not.toBe(18)
 	})
 })
