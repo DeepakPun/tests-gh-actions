ab245ae6a80afc600cb58101de1de018d3a6512e
Deepak Pun <pun.deepak1986@gmail.com>
2025-01-06 19:14:30 -0500

    All tests pass now

--- __tests__/sample.test.js
+++ __tests__/sample.test.js
@@ -5,19 +5,19 @@ describe('test utility functions defined in funcs file', () => {
 		expect(add(6, 6)).toBe(12)
 	})
 
-	test('adds 6 + 6 to equal 12', () => {
+	test('adds 7 + 8 to equal 12', () => {
 		expect(add(7, 8)).toBe(15)
 	})
 
-	test('adds 6 + 5 to not equal 12', () => {
-		expect(add(6, 5)).not.toBe(12)
+	test('adds 6 + 5 to equal 11', () => {
+		expect(add(6, 5)).toBe(11)
 	})
 
 	test('subtract 10 - 4 to equal 6', () => {
 		expect(diff(10, 4)).toBe(6)
 	})
 
-	test('subtract 20 - 7 to not equal 13', () => {
-		expect(diff(20, 7)).not.toBe(18)
+	test('subtract 20 - 7 to equal 13', () => {
+		expect(diff(20, 7)).toBe(13)
 	})
 })
